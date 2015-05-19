294c12c8dc92bf362a9e862bf2a32749afdfb218
Christopher West <9ninemind@gmail.com>
2015-05-19 11:19:31 -0600

    adds BundleException

---
+++ Exception/PersonBundleException.php
@@ -0,0 +1,9 @@
+<?php
+namespace BlackBoxCode\Pando\PersonBundle\Exception;
+
+use BlackBoxCode\Pando\BaseBundle\Exception\BaseException;
+
+class PersonBundleException extends BaseException
+{
+
+}
